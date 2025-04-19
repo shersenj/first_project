46d311d100cc9256d81f01c118b363f727dd2a72
Shershenj <kishevoy11@gmail.com>
2025-04-19 12:42:51 +0300

    v-0.1a

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # first_project
+Синхронизация с github
+    - git add .
+    - git commit - m 'comment'
+    - git push
+    - git status (посмотреть изменения)
--- practicum.py
+++ practicum.py
@@ -3,24 +3,58 @@ from random import randint
 
 table = PrettyTable()
 number = randint(1, 100)
-count = 0
 
-print('Угадай число от 1 до 100')
 
-while True:
-    guess = int(input('Введите целое число от 1 до 100 число: '))
-    table.field_names = ['Попытка', 'Число', 'Результат']
-    if guess < number:
-        count += 1
-        table.add_row([str(count), str(guess), 'Меньше'])
-        print('Ваше число меньше того, что загаданно.')
-    if guess > number:
-        count += 1
-        table.add_row([str(count), str(guess), 'Больше'])
-        print('Ваше число больше того, что загаданно.')
-    if guess == number:
-        count += 1
-        table.add_row([str(count), str(guess), 'БИНГО!!!'])
-        break
-print('Отличная интуиция! Вы угадали число :) С ' + str(count) + ' попыток!')
-print(table)
+print('''
+      Угадай число!
+      Для победы угадай с 5 попыток
+      За победу + 10 за поражение - 10
+      ''')
+
+
+def guess_the_number():
+    '''Игра угадай число!'''
+
+    count = 0
+    balance = 100
+
+    while True:
+        menu = input('go для начала, no для выхода: ')
+        if menu != 'go' and menu != 'no':
+            print('Неверная команда')
+            continue
+        if menu == 'no':
+            print('Приходи ещё!')
+            break
+        if menu == 'go':
+            while True:
+                guess = int(input('Введите целое число от 1 до 100 число: '))
+                table.field_names = ['Попытка', 'Число', 'Результат']
+                if guess < number:
+                    count += 1
+                    table.add_row([str(count), str(guess), 'Меньше'])
+                    print('Ваше число меньше того, что загаданно.')
+                if guess > number:
+                    count += 1
+                    table.add_row([str(count), str(guess), 'Больше'])
+                    print('Ваше число больше того, что загаданно.')
+                if guess == number:
+                    count += 1
+                    table.add_row([str(count), str(guess), 'БИНГО!!!'])
+                    if count <= 5:
+                        balance += 10
+                    else:
+                        balance -= 10
+                    break
+        print(
+            'Отличная интуиция! Вы угадали число :)'
+            'С ' + str(count) + ' попыток!')
+        count = 0
+        print(balance)
+        print(table)
+
+        if menu == 'no':
+            break
+
+
+guess_the_number()
